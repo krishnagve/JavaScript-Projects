11c4ae89bd26461260619bfe330fb8a299a17faa
krishnagve <krishna4ve@gmail.com>
2025-11-03 15:09:33 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// Display a popup message using the window.alert() method
+window.alert("Welcome! This is a simple JavaScript program.");
+
+// Create two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Display the combined string on the webpage
+document.write(Sent1 + Sent2);
+document.write("<br><br>"); // Add two line breaks for spacing
+
+// Create multiple variables in one statement (family members)
+var Family = "The Arezzinis", 
+    Dad = "Jeremiah", 
+    Mom = "Hermoine", 
+    Daughter = "Penny", 
+    Son = "Zorro";
+
+// Display one of the family members on the page
+document.write(Dad);
+document.write("<br><br>"); // Add space before next output
+
+// Write an expression example (simple math)
+document.write("The result of 5 + 3 is: " + (5 + 3));
